38c32bcaf5c59c6c18ae365b8743ca57504c0f15
Eric Montzka <emontzka@jnlcom.com>
2023-11-08 12:04:56 -0600

    add bugherd plugin, adjust newsletter

--- ag-site-acf-blocks/blocks/ncff-newsletter/ncff-newsletter.php
+++ ag-site-acf-blocks/blocks/ncff-newsletter/ncff-newsletter.php
@@ -16,12 +16,25 @@ $link = get_field('newsletter_link', 'options');
 <div id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?>">
 <div class="ncff-newsletter__text-area">
     <h3 class="h2">Read<span class="ampersand">&</span>Connect </h3>
-    <?php echo get_the_post_thumbnail( $link, 'full');
+    <?php echo '<a href="'.get_the_permalink($link).'">'.get_the_post_thumbnail( $link, 'full').'</a>';
     echo $text; ?> 
 </div>
 <div class="ncff-newsletter__form-area">
 <p>Get the lastest news, recipes, articles and more, right to your inbox.</p>
 <?php gravity_form( 3, false, false, false, '', false ); ?>
+<p style="margin-bottom: .5em">Connect with us</p>
+<ul class="newsletter-social">
+    <?php
+    $facebook = get_field('facebook', 'options');
+    $twitter = get_field('twitter', 'options');
+    $pinterest = get_field('pinterest', 'options');
+    $youtube = get_field('youtube', 'options');
+        echo $facebook ? '<li class="facebook"><a href="'.esc_url($facebook).'" target="_blank"><i class="fab fa-facebook-f"></i></a></li>' : '';
+        echo $twitter ? '<li class="twitter"><a href="'.esc_url($twitter).'" target="_blank"><i class="fab fa-twitter"></i></a></li>' : '';
+        echo $pinterest ? '<li class="pinterest"><a href="'.esc_url($pinterest).'" target="_blank"><i class="fab fa-pinterest-p"></i></a></li>' : '';
+        echo $youtube ? '<li class="youtube"><a href="'.esc_url($youtube).'" target="_blank"><i class="fab fa-youtube"></i></a></li>' : '';
+    ?>
+</ul>
 </div>
 </div>
 </div> <!-- container -->
