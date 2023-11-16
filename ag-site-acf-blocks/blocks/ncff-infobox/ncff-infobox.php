b61179df0f03bb1e8b2d24d3192123dfc8ed72fb
Eric Montzka <emontzka@jnlcom.com>
2023-11-16 08:30:31 -0600

    finish infobox

--- ag-site-acf-blocks/blocks/ncff-infobox/ncff-infobox.php
+++ ag-site-acf-blocks/blocks/ncff-infobox/ncff-infobox.php
@@ -10,11 +10,18 @@ if( !empty($block['className']) ) {
 } 
 
 $infobox = get_field('infobox');
+$link = get_field('link');
 ?>
 
 <div id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?>">
 <div class="ncff-infobox__container">
-    <?php echo $infobox; ?>
+    <?php echo $infobox; 
+    if ($link) {
+        $text = $link['title'] ? $link['title'] : "Learn More";
+        echo '<div style="text-align: center;">';
+        echo '<a href="'.$link['url'].'" target="'.$link['target'].'" ><button class="background__primary">'.$text.'</button></a>';
+        echo '</div>';
+    } ?>
 </div>
 </div>
 
