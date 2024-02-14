e1f69112b4d0c7c48b01f81b3d8ace3dde189ac2
Eric Montzka <emontzka@jnlcom.com>
2024-02-14 14:18:24 -0600

    recipe fixes

--- jci-ff-recipe-manager/block/ncff-recipe-block.php
+++ jci-ff-recipe-manager/block/ncff-recipe-block.php
@@ -19,10 +19,14 @@ $recipe_img_url = get_field('recipe_image_link', $recipe_id);
 ?>
 
 <div id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className);  ?>">
-    <h3 class="page-header">Recipe</h3>
-    <?php //echo get_the_post_thumbnail($recipe_id ); ?>
+<div class="ncff-recipe-block__img-container">
     <img src="<?php echo esc_attr($recipe_img_url ); ?>" />
-    <?php echo '<h3 class="recipe-block-title">'.get_the_title($recipe_id ).'</h3>'; ?>
-    <?php echo '<div class="recipe-block-text">'.$text.'</div>';  ?>
-    <a href="<?php echo esc_url($recipe_url ); ?>"><button>See the recipe</button></a>
+</div>
+<div class="ncff-recipe-block__text-container">
+    <img class="ncff-recipe-block__icon" src="<?php echo plugin_dir_url(__FILE__); ?>../images/farm-flavor-icon-150x150.png" alt="">
+    <?php echo '<h3 class="ncff-recipe-block__title">'.get_the_title($recipe_id ).'</h3>'; ?>
+    <?php echo '<div class="ncff-recipe-block__text">'.$text.'</div>';  ?>
+    <a href="<?php echo esc_url($recipe_url ); ?>"><button class="ncff-recipe-block__button">See the recipe on Farmflavor.com</button></a>
+</div>
+    
 </div>
