41a9ba3658bae40b93b18430cca00711436b8622
emontzka <emontzka@jnlcom.com>
2024-09-25 14:16:27 -0500

    change block layout

--- ag-site-acf-blocks/blocks/magazine-social/magazine-social.php
+++ ag-site-acf-blocks/blocks/magazine-social/magazine-social.php
@@ -22,13 +22,15 @@ $twitter = get_field('twitter', 'options');
 $pinterest = get_field('pinterest', 'options');
 ?>
 <div id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?>" style="margin-bottom: 80px;">
-    <div class="container jcims__container">
+    <div class="container">
+        <div class="jcims__container">
         <div class="jcims__mag-container">
             <a href="<?php echo site_url( 'magazine'); ?>">
             <?php echo get_the_post_thumbnail( $magPost->ID, 'medium_large' ,array('style' => 'width: 200px; height: auto;')); ?>
             </a>
             <div class="jcims__mag-text">
             <h3 class="handwritten">the magazine</h3>
+            <hr />
             <?php echo $magPost->post_content; ?>
             <!-- <a href="<?php //echo site_url( 'magazine'); ?>"><button>Latest Issue</button></a> -->
             </div>
@@ -44,6 +46,7 @@ $pinterest = get_field('pinterest', 'options');
                         echo $pinterest ? '<li class="pinterest"><a href="'.esc_url($pinterest).'" target="_blank"><svg style="max-width: 25px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M204 6.5C101.4 6.5 0 74.9 0 185.6 0 256 39.6 296 63.6 296c9.9 0 15.6-27.6 15.6-35.4 0-9.3-23.7-29.1-23.7-67.8 0-80.4 61.2-137.4 140.4-137.4 68.1 0 118.5 38.7 118.5 109.8 0 53.1-21.3 152.7-90.3 152.7-24.9 0-46.2-18-46.2-43.8 0-37.8 26.4-74.4 26.4-113.4 0-66.2-93.9-54.2-93.9 25.8 0 16.8 2.1 35.4 9.6 50.7-13.8 59.4-42 147.9-42 209.1 0 18.9 2.7 37.5 4.5 56.4 3.4 3.8 1.7 3.4 6.9 1.5 50.4-69 48.6-82.5 71.4-172.8 12.3 23.4 44.1 36 69.3 36 106.2 0 153.9-103.5 153.9-196.8C384 71.3 298.2 6.5 204 6.5z"/></svg></a></li>' : '';
                     ?>
                 </ul>
+        </div>
         </div>
     </div>
 </div>
