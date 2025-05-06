28af51a5e807e387f1900cff242f61c99f2dae04
emontzka <emontzka@jnlcom.com>
2025-05-06 16:41:56 -0500

    add cat link to pop post

--- fff-acf-blocks/blocks/fff-popular-posts/fff-popular-posts.php
+++ fff-acf-blocks/blocks/fff-popular-posts/fff-popular-posts.php
@@ -15,9 +15,9 @@ $args = array(
     'paginated'             => 1,
     'paged'                 => $paged,
     'post_status'           => 'publish',
-    // 'orderby'               => 'meta_value_num',
-    // 'order'                 => 'DESC',
-    // 'meta_key'              => 'wpb_post_views_count'
+    'orderby'               => 'meta_value_num',
+    'order'                 => 'DESC',
+    'meta_key'              => 'wpb_post_views_count'
 );
 $pop_query = new WP_Query($args); ?>
 <div id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?>">
@@ -31,6 +31,7 @@ $pop_query = new WP_Query($args); ?>
         $cat = get_the_category($ID); 
         $primary_cat = get_post_meta($ID,'_yoast_wpseo_primary_category', TRUE ); 
         $cat_name = $primary_cat ? get_the_category_by_ID($primary_cat) : $cat[0]->name;
+        $cat_link = $primary_cat ? get_category_link( $primary_cat) : get_category_link( $cat[0]->term_id );
         ?>
         <div class="col-12 m-col-4 s-col-6">
         <div class="fff-popular__container  nc-panel">
@@ -38,7 +39,7 @@ $pop_query = new WP_Query($args); ?>
                 <?php echo '<a href="'.get_the_permalink().'">'.get_the_post_thumbnail( ).'</a>'; ?>
             </div>
             <div class="fff-popular__text-container">
-            <p class="fff-category"><?php echo $cat_name; ?></p>
+            <p class="fff-category"><?php echo '<a href="'.$cat_link.'">'.$cat_name.'</a>'; ?></p>
             <h3 class="fff-popular__title"><a class="unstyle-link" href="<?php echo get_the_permalink(); ?>"><?php echo get_the_title(); ?></a></h3>
             </div>
             </div> <!-- container -->
